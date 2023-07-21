101c061b1cc1c5170fe878b3c4dfc3cee5fc5f04
coDev <jh900127@gmail.com>
2023-07-21 12:46:44 +0900

    kakao login, sign

--- users/views.py
+++ users/views.py
@@ -142,7 +142,7 @@ class GitHubLogIn(APIView):
         try:
             code = request.data.get("code")
             access_token = requests.post(
-                f"https://github.com/login/oauth/access_token?code={code}&client_id=bc90b24233a834d6a1a6&client_secret={settings.GH_SECRET}",
+                f"https://github.com/login/oauth/access_token?code={code}&client_id={settings.GH_CLIENT_ID}&client_secret={settings.GH_SECRET}",
                 headers={"Accept": "application/json"},
             )
             access_token = access_token.json().get("access_token")
@@ -172,6 +172,53 @@ class GitHubLogIn(APIView):
                     email=user_email[0]["email"],
                     name=user_data.get("name"),
                     avatar=user_data.get("avatar_url"),
+                )
+                # SNS 가입 유저는 패스워드 사용 불가 처리
+                user.set_unusable_password()
+                user.save()
+                login(request, user)
+                return Response(status=status.HTTP_200_OK)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class KakaoLogIn(APIView):
+    def post(self, request):
+        try:
+            code = request.data.get("code")
+            access_token = requests.post(
+                "https://kauth.kakao.com/oauth/token",
+                headers={
+                    "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+                },
+                data={
+                    "grant_type": "authorization_code",
+                    "client_id": "145c7d42253edad1ff05752f940f5878",
+                    "redirect_uri": "http://127.0.0.1:3000/social/kakao",
+                    "code": code,
+                },
+            )
+            access_token = access_token.json().get("access_token")
+            user_data = requests.get(
+                "https://kapi.kakao.com/v2/user/me",
+                headers={
+                    "Authorization": f"Bearer {access_token}",
+                    "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+                },
+            )
+            user_data = user_data.json()
+            kakao_account = user_data.get("kakao_account")
+            profile = kakao_account.get("profile")
+            try:
+                user = User.objects.get(email=kakao_account.get("email"))
+                login(request, user)
+                return Response(status=status.HTTP_200_OK)
+            except User.DoesNotExist:
+                user = User.objects.create(
+                    username=profile.get("nickname"),
+                    email=kakao_account.get("email"),
+                    name=profile.get("nickname"),
+                    avatar=profile.get("profile_image_url"),
                 )
                 # SNS 가입 유저는 패스워드 사용 불가 처리
                 user.set_unusable_password()
